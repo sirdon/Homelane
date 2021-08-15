67ac7a4191a0e0191aa6e3a86828cd073020d3a3
sirdon <inform2theamit@gmail.com>
2021-08-15 21:02:49 +0530

    changes

--- routes/HomelaneApi/route.js
+++ routes/HomelaneApi/route.js
@@ -1,8 +1,8 @@
 import express from "express";
 const homelameRoutes = express.Router();
 import files from "./index.js";
-homelameRoutes.get("/Get_data_info", files.get.getDataInfo);
+homelameRoutes.get("/Get_date_info", files.get.getDateInfo);
 homelameRoutes.get("/Get_state_info", files.get.getStateInfo);
 homelameRoutes.get("/Pinpoint_state", files.get.pinPointState);
-homelameRoutes.get("/Pinpoint_state_date", files.get.pinPointStateInfo);
+homelameRoutes.get("/Pinpoint_info", files.get.pinPointInfo);
 export default homelameRoutes;
