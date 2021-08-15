aca691f245a70375bd7b71a49ec90b84510856b0
sirdon <inform2theamit@gmail.com>
2021-08-15 23:24:53 +0530

    ex

--- routes/HomelaneApi/route.js
+++ routes/HomelaneApi/route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const homelameRoutes = express.Router();
 import files from "./index.js";
-homelameRoutes.get("/Get_state_infoGet_date_info", files.get.getDateInfo);
+homelameRoutes.get("/Get_state_info", files.get.getDateInfo);
 homelameRoutes.get("/Get_date_info", files.get.getStateInfo);
 homelameRoutes.get("/Pinpoint_state", files.get.pinPointState);
 homelameRoutes.get("/Pinpoint_info", files.get.pinPointInfo);
